83ea4a0ae162eca2d4baf26ba390bb6c76645a95
TrueZeroOne <43008963+TrueZeroOne@users.noreply.github.com>
2022-01-30 22:08:07 +0100

    Update with some seudocode

--- TerminsProeve/TerminsProeve/Form1.cs
+++ TerminsProeve/TerminsProeve/Form1.cs
@@ -12,6 +12,7 @@ namespace TerminsProeve
 {
     public partial class Form1 : Form
     {
+        int story = 0;
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +23,42 @@ namespace TerminsProeve
             label1.Text = "Bungus";
             label1.Location = new Point(-30, 50);
             label1.Font = new Font(label1.Font.FontFamily, 170);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(story == 0)
+            {
+                //Insert Option One Story here
+                story += 1;
+                //button1.Text = "Insert Next option";
+                //button2.Text = "Insert Next option";
+                //button3.Text = "Insert Next option";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (story == 0)
+            {
+                //Insert Option Two Story here
+                story += 2;
+                //button1.Text = "Insert Next option";
+                //button2.Text = "Insert Next option";
+                //button3.Text = "Insert Next option";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (story == 0)
+            {
+                //Insert Option Three Story here
+                story += 3;
+                //button1.Text = "Insert Next option";
+                //button2.Text = "Insert Next option";
+                //button3.Text = "Insert Next option";
+            }
         }
     }
 }
