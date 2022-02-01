ff14cb78bf496d76d585904cf12814d0725b3826
TrueZeroOne <43008963+TrueZeroOne@users.noreply.github.com>
2022-02-01 15:23:10 +0100

    Text ting...

--- TerminsProeve/TerminsProeve/Form1.cs
+++ TerminsProeve/TerminsProeve/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TerminsProeve
 {
@@ -16,6 +17,9 @@ namespace TerminsProeve
         public Form1()
         {
             InitializeComponent();
+            string path = @"C:\Users\blaab\Documents\GitHub\TerminsProeve\TerminsProeve\TerminsProeve\Properties\StoryFile.txt";
+            string text = File.ReadAllText(path);
+            string[]textArray = text.Split('|');
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -23,6 +27,10 @@ namespace TerminsProeve
             label1.Text = "Bungus";
             label1.Location = new Point(-30, 50);
             label1.Font = new Font(label1.Font.FontFamily, 170);
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            textBox1.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -37,30 +45,34 @@ namespace TerminsProeve
             }
             if (story == 1)
             {
-                //Insert Option 1,1 story here
-                story += 10;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 1,1 here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
             if (story == 2)
             {
-                //Insert Option 2,1 story here
-                story += 10;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 2,1 here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
             if (story == 3)
             {
-                //Insert Option 3,1 story here
-                story += 10;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 3,1  here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
-
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -75,27 +87,33 @@ namespace TerminsProeve
             }
             if (story == 1)
             {
-                //Insert Option 1,2 story here
-                story += 20;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 1,2 here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
             if (story == 2)
             {
-                //Insert Option 2,2 story here
-                story += 20;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 2,2 here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
             if (story == 3)
             {
-                //Insert Option 3,2 story here
-                story += 20;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 3,2  here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
         }
 
@@ -106,34 +124,56 @@ namespace TerminsProeve
             {
                 //Insert Option Three Story here
                 story += 3;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                button1.Text = "Climb the mountain";
+                button2.Text = "Jump over it";
+                button3.Text = "Go around it";
             }
             if (story == 1)
             {
-                //Insert Option 1,3 story here
-                story += 30;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 1,3 here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
             if (story == 2)
             {
-                //Insert Option 2,3 story here
-                story += 30;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 2,3 here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
             if (story == 3)
             {
-                //Insert Option 3,3 story here
-                story += 30;
-                //button1.Text = "Insert Next option";
-                //button2.Text = "Insert Next option";
-                //button3.Text = "Insert Next option";
+                //Insert Ending 3,3  here
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Location = button2.Location;
+                button4.Size = button2.Size;
+                button4.Font = button2.Font;
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+            label2.Visible = false;
+            textBox1.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+            //Muligvis pop up om spiller er sikker
         }
     }
 }
