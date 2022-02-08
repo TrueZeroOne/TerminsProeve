bd52175e2d3ba3ebd5451bc0e33e98a55ae93294
TrueZeroOne <43008963+TrueZeroOne@users.noreply.github.com>
2022-02-08 15:02:39 +0100

    Well here we go again

--- TerminsProeve/TerminsProeve/Form1.cs
+++ TerminsProeve/TerminsProeve/Form1.cs
@@ -18,39 +18,53 @@ namespace TerminsProeve
         public Form1()
         {
             InitializeComponent();
+            // Fetches story from Text file and spilts it up in the diffrent parts of story in an array 
             string path = @"StoryFile.txt";
             string text = File.ReadAllText(path);
             textArray = text.Split('|');
             textBox1.Text = textArray[0];
         }
-
+        // When Label is clicked
         private void label1_Click(object sender, EventArgs e)
         {
+            // Changes its text to bungus
             label1.Text = "Bungus";
-            label1.Location = new Point(-30, 50);
+            // Sets the location to a new point at the left of the screen
+            label1.Location = new Point(-30, 40);
+            // Sets the font to be size 170
             label1.Font = new Font(label1.Font.FontFamily, 170);
+            // Hides buttons and textbox
             button1.Visible = false;
             button2.Visible = false;
             button3.Visible = false;
             textBox1.Visible = false;
         }
-
+        // checks if Button one which is the button to the left is clicked
         private void button1_Click(object sender, EventArgs e)
         {
+            // if statements check where in the story it is 
+
+            //This is the first part right after you press start
             if (story == 0)
             {
+                // Sets the textbox content to be the story if you choose option 1
                 textBox1.Text = textArray[1];
+                // story progresion to keep track of which part of the story we are in
                 story += 1;
+                //Sets the text of the buttons to the next option
                 button1.Text = "Heaven";
                 button2.Text = "Home";
                 button3.Text = "Hell";
             }
             else if (story == 1)
             {
+                // Sets the textbox content to be the story if you choose option 1
                 textBox1.Text = textArray[2];
+                // Hides the buttons
                 button1.Visible = false;
                 button2.Visible = false;
                 button3.Visible = false;
+                // Moves and resizes the quit button to the middle button
                 button5.Location = button2.Location;
                 button5.Size = button2.Size;
                 button5.Font = button2.Font;
